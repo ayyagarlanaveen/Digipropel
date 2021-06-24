09f48293ddb3de10847d09609c10c84cf4b57608
ayyagarlanaveen <86399765+ayyagarlanaveen@users.noreply.github.com>
2021-06-24 14:07:48 +0530

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+    int i=251152;
+  static void Main() {
+      
+    Console.WriteLine("Hello World");
+    Console.WriteLine("Ayyagarla Naveen your id is following below");
+    HelloWorld HelloWorld=new HelloWorld();
+    Console.WriteLine(HelloWorld.i);
+    
+  }
+}
