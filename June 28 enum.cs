c3096890b839351cf53f78f1d3253c5262a9fe3b
ayyagarlanaveen <86399765+ayyagarlanaveen@users.noreply.github.com>
2021-07-01 19:18:45 +0530

    Add files via upload

---
+++ June 28 enum.cs
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+namespace DemoApplication
+{
+ class Program 
+ {
+  enum Days{Sun,Mon,tue,Wed,thu,Fri,Sat};
+  
+  static void Main(string[] args) 
+  {
+   Console.Write(Days.Sun);
+   
+   Console.ReadKey();
+  }
+ }
+}
